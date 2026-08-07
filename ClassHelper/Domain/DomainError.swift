de339f4bb430a7c2f1f7cfd69ed7273950c03e94
p1001q <qrwrer123@gmail.com>
2026-08-07 09:36:06 +0900

    feat: local processing 상태 전이 규칙 추가 (#9)

--- ClassHelper/Domain/DomainError.swift
+++ ClassHelper/Domain/DomainError.swift
@@ -5,4 +5,8 @@
 
 nonisolated enum DomainError: Error, Equatable, Sendable {
     case invalidRecordingTransition(state: RecordingState, command: SessionCommand)
+    case invalidLocalProcessingTransition(
+        state: LocalProcessingState,
+        command: LocalProcessingCommand
+    )
 }
---
+++ ClassHelper/Domain/LocalProcessingTransitionPolicy.swift
@@ -0,0 +1,62 @@
+//
+//  LocalProcessingTransitionPolicy.swift
+//  ClassHelper
+//
+
+nonisolated enum LocalProcessingTransitionPolicy {
+    static func transition(
+        from state: LocalProcessingState,
+        command: LocalProcessingCommand
+    ) throws -> LocalProcessingState {
+        switch (state, command) {
+        case (.capturing, .captureEndedSafely):
+            return .finalizingTranscript
+        case (.finalizingTranscript, .finalizedTranscriptVerified):
+            return .generatingNote
+        case (.generatingNote, .generatedNoteVerified):
+            return .savingLocal
+        case (.savingLocal, .canonicalNoteReadBackVerified):
+            return .localComplete
+        case (
+            .capturing,
+            .recoverableFailureVerified
+        ), (
+            .finalizingTranscript,
+            .recoverableFailureVerified
+        ), (
+            .generatingNote,
+            .recoverableFailureVerified
+        ), (
+            .savingLocal,
+            .recoverableFailureVerified
+        ):
+            return .recoverableFailed
+        case (
+            .capturing,
+            .unrecoverableFailureVerified
+        ), (
+            .finalizingTranscript,
+            .unrecoverableFailureVerified
+        ), (
+            .generatingNote,
+            .unrecoverableFailureVerified
+        ), (
+            .savingLocal,
+            .unrecoverableFailureVerified
+        ):
+            return .unrecoverableFailed
+        case (.recoverableFailed, .retryFromUsableAudio):
+            return .finalizingTranscript
+        case (.recoverableFailed, .retryFromVerifiedTranscript):
+            return .generatingNote
+        case (.recoverableFailed, .retryFromValidGeneratedNote):
+            return .savingLocal
+        case (.recoverableFailed, .reconcileVerifiedFinalNote):
+            return .localComplete
+        case (.recoverableFailed, .confirmedDiscardCompletedAndVerified):
+            return .discarded
+        default:
+            throw DomainError.invalidLocalProcessingTransition(state: state, command: command)
+        }
+    }
+}
